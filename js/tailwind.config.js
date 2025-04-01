32f0aa704ac59b2f4a26de262e9fd1e4e8b7fdf3
edheer <edheer@student.che.nl>
2025-04-01 19:59:19 +0200

    Tailwind.css added

---
+++ js/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    content: [
+      "./*.html",
+      "./**/*.html",
+      "./**/*.js"
+    ],
+    theme: {
+      extend: {
+        colors: {
+          donkerblauw: "#0f172a",
+          beige: "#fcd9c4",
+          paars: "#c084fc",
+        },
+        fontFamily: {
+          mono: ["'Anonymous Pro'", "monospace"],
+          serif: ["'Playfair Display'", "serif"]
+        }
+      },
+    },
+    plugins: [],
+  }
+  
